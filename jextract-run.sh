03259abf28a53a9be759c1bb6e233789fe611ba6
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2026-04-27 13:11:22 +0000

    Updated jextract script

--- jextract-run.sh
+++ jextract-run.sh
@@ -3,5 +3,5 @@ jextract \
   --output src/main/java \
   -l ffm_kata \
   -t com.evolutionnext.foreignfunctionmemory.generated \
-  -I /Users/danno/Development/mine/java_enable_foreign_function_memory_api/projects/ffm_kata \
+  -I projects/ffm_kata \
   projects/ffm_kata/ffm_kata.h
